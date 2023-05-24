db714d676a78f5ecb0acba58ced3ce5d026b14ff
Luciano Anselmino <luciano.anselmino@gmail.com>
2023-05-24 01:56:18 -0300

    "mostrar timepo hasta proxima pregunta"

--- assets/js/cuestionario.js
+++ assets/js/cuestionario.js
@@ -1,46 +1,88 @@
 function iniciarCuestionario() {
 
-    fetch('https://opentdb.com/api.php?amount=3&category=18&difficulty=medium&type=boolean')
-      .then(response => response.json())
-      .then(data => {
-        traducirPreguntas(data.results);
-      })
-      .catch(error => {
-        console.error('Error al obtener las preguntas:', error);
-      });
-  
-    document.getElementById('cuestionario').style.display = 'block';
-    document.getElementById('resultado').style.display = 'none';
-  }
-
-  function traducirTexto(texto) {
-    const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(texto)}&langpair=en|es`;
-    return fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        return data.responseData.translatedText.replace(/&quot;/g, ' ');
-      });
-  }
-
-  
-  function traducirPreguntas(preguntas) {
-    const promesasTraduccion = preguntas.map(pregunta => {
-      return traducirTexto(pregunta.question)
-        .then(preguntaTraducida => {
-          pregunta.traducida = preguntaTraducida; // Agregar la propiedad "traducida" a la pregunta
-          return pregunta;
-        });
+  fetch('https://opentdb.com/api.php?amount=3&category=18&difficulty=medium&type=boolean')
+    .then(response => response.json())
+    .then(data => {
+      traducirPreguntas(data.results);
+    })
+    .catch(error => {
+      console.error('Error al obtener las preguntas:', error);
     });
+
+  document.getElementById('cuestionario').style.display = 'block';
+  document.getElementById('resultado').style.display = 'none';
+}
+
+/*function traducirTexto(texto) {
+  const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(texto)}&langpair=en|es`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {console.log(data);})
+  }*/
+
+ 
+function traducirTexto(texto) {
+  const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(texto)}&langpair=en|es`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.responseStatus === 200) {
+        return data.responseData.translatedText.replace(/&quot;/g, ' ');
+      } else if (data.responseStatus === 429 && data.responseDetails) {
+        const warningMessage = data.responseDetails;
+        const mensaje = mostrarMensajeLimite(warningMessage);
+        return mensaje;
+      } else {
+        throw new Error('Error en la traducción');
+      }
+    })
+    .catch((error) => {
+      console.error('Error en la solicitud de traducción:', error);
+      throw error; // Volver a lanzar el error para que sea capturado en el catch de calificarRespuestas
+    });
+}
+
+function mostrarMensajeLimite(warningMessage) {
+  const regex = /NEXT AVAILABLE IN\s+(\d+) HOURS (\d+) MINUTES (\d+) SECONDS/;
+  const matches = regex.exec(warningMessage);
   
-    Promise.all(promesasTraduccion)
-      .then(preguntasTraducidas => {
-        mostrarPreguntas(preguntasTraducidas);
+  if (matches) {
+    const HOURS = parseInt(matches[1]);
+    const MINUTES = parseInt(matches[2]);
+    const SECONDS = parseInt(matches[3]);
+
+    const mensajeSalida = `Próxima pregunta disponible en: ${HOURS} horas ${MINUTES} minutos ${SECONDS} segundos`;
+    return mensajeSalida;
+  } else {
+    return 'Mensaje de traducción inválido';
+  }
+}
+    
+
+function traducirPreguntas(preguntas) {
+  const promesasTraduccion = preguntas.map(pregunta => {
+    return traducirTexto(pregunta.question)
+      .then(preguntaTraducida => {
+        pregunta.traducida = preguntaTraducida; // Agregar la propiedad "traducida" a la pregunta
+        return pregunta;
       })
       .catch(error => {
-        console.error('Error al traducir las preguntas:', error);
+        console.error('Error al traducir la pregunta:', error);
+        return pregunta; // Devolver la pregunta original si hay un error de traducción
       });
-  }
-  
+  });
+
+  Promise.all(promesasTraduccion)
+    .then(preguntasTraducidas => {
+      mostrarPreguntas(preguntasTraducidas);
+    })
+    .catch(error => {
+      console.error('Error al traducir las preguntas:', error);
+    });
+}
+ 
+
+  //mostrar preguntas
   function mostrarPreguntas(preguntas) {
     const contenedorPreguntas = document.getElementById('preguntas');
     contenedorPreguntas.innerHTML = '';
